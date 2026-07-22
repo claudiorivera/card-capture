6856023e31a9703caefe4205c7f25eb2bafa8c90
Claudio Rivera <me@claudiorivera.com>
2026-07-21 21:18:06 -0500

    🔥 Remove extraneous wrapper

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,9 +4,5 @@ import { GameBoard } from "#/components/game-board";
 export const Route = createFileRoute("/")({ component: Home, ssr: false });
 
 function Home() {
-	return (
-		<div className="flex flex-col gap-4 p-4">
-			<GameBoard />
-		</div>
-	);
+	return <GameBoard />;
 }
